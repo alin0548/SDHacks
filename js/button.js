e4a47fc6c585e3d6526473df182bab2537189555
Se7ensins1 <anastasiavela@berkeley.edu>
2017-10-21 14:32:04 -0700

    move alphabet

--- js/button.js
+++ js/button.js
@@ -1,42 +1,42 @@
 // requirements
-var ac = readTextFile("./static/requirement2classes/ac.txt");
-var ah = readTextFile("./static/requirement2classes/ah.txt");
-var ai = readTextFile("./static/requirement2classes/ai.txt");
-var al = readTextFile("./static/requirement2classes/al.txt");
-var bs = readTextFile("./static/requirement2classes/bs.txt");
-var elw = readTextFile("./static/requirement2classes/elw.txt");
-var hs = readTextFile("./static/requirement2classes/hs.txt");
-var is = readTextFile("./static/requirement2classes/is.txt");
-var ps = readTextFile("./static/requirement2classes/ps.txt");
-var pv = readTextFile("./static/requirement2classes/pv.txt");
-var r1a = readTextFile("./static/requirement2classes/r1a.txt");
-var r1b = readTextFile("./static/requirement2classes/r1b.txt");
-var sbs = readTextFile("./static/requirement2classes/sbs.txt");
+var ac = readTextFile("../static/requirement2classes/ac.txt");
+var ah = readTextFile("../static/requirement2classes/ah.txt");
+var ai = readTextFile("../static/requirement2classes/ai.txt");
+var al = readTextFile("../static/requirement2classes/al.txt");
+var bs = readTextFile("../static/requirement2classes/bs.txt");
+var elw = readTextFile("../static/requirement2classes/elw.txt");
+var hs = readTextFile("../static/requirement2classes/hs.txt");
+var is = readTextFile("../static/requirement2classes/is.txt");
+var ps = readTextFile("../static/requirement2classes/ps.txt");
+var pv = readTextFile("../static/requirement2classes/pv.txt");
+var r1a = readTextFile("../static/requirement2classes/r1a.txt");
+var r1b = readTextFile("../static/requirement2classes/r1b.txt");
+var sbs = readTextFile("../static/requirement2classes/sbs.txt");
 
-var dept2acro = readTextFile(".static/full_to_acr.txt");
+var dept2acro = readTextFile("../static/full_to_acr.txt");
 
-var acro2classA = readTextFile("./static/acronym2classes/A.txt");
-var acro2classB = readTextFile("./static/acronym2classes/B.txt");
-var acro2classC = readTextFile("./static/acronym2classes/C.txt");
-var acro2classD = readTextFile("./static/acronym2classes/D.txt");
-var acro2classE = readTextFile("./static/acronym2classes/E.txt");
-var acro2classF = readTextFile("./static/acronym2classes/F.txt");
-var acro2classG = readTextFile("./static/acronym2classes/G.txt");
-var acro2classH = readTextFile("./static/acronym2classes/H.txt");
-var acro2classI = readTextFile("./static/acronym2classes/I.txt");
-var acro2classJ = readTextFile("./static/acronym2classes/J.txt");
-var acro2classK = readTextFile("./static/acronym2classes/K.txt");
-var acro2classL = readTextFile("./static/acronym2classes/L.txt");
-var acro2classM = readTextFile("./static/acronym2classes/M.txt");
-var acro2classN = readTextFile("./static/acronym2classes/N.txt");
-var acro2classO = readTextFile("./static/acronym2classes/O.txt");
-var acro2classP = readTextFile("./static/acronym2classes/P.txt");
-var acro2classR = readTextFile("./static/acronym2classes/R.txt");
-var acro2classS = readTextFile("./static/acronym2classes/S.txt");
-var acro2classT = readTextFile("./static/acronym2classes/T.txt");
-var acro2classU = readTextFile("./static/acronym2classes/U.txt");
-var acro2classV = readTextFile("./static/acronym2classes/V.txt");
-var acro2classY = readTextFile("./static/acronym2classes/Z.txt");
+var acro2classA = readTextFile("../static/acronym2classes/A.txt");
+var acro2classB = readTextFile("../static/acronym2classes/B.txt");
+var acro2classC = readTextFile("../static/acronym2classes/C.txt");
+var acro2classD = readTextFile("../static/acronym2classes/D.txt");
+var acro2classE = readTextFile("../static/acronym2classes/E.txt");
+var acro2classF = readTextFile("../static/acronym2classes/F.txt");
+var acro2classG = readTextFile("../static/acronym2classes/G.txt");
+var acro2classH = readTextFile("../static/acronym2classes/H.txt");
+var acro2classI = readTextFile("../static/acronym2classes/I.txt");
+var acro2classJ = readTextFile("../static/acronym2classes/J.txt");
+var acro2classK = readTextFile("../static/acronym2classes/K.txt");
+var acro2classL = readTextFile("../static/acronym2classes/L.txt");
+var acro2classM = readTextFile("../static/acronym2classes/M.txt");
+var acro2classN = readTextFile("../static/acronym2classes/N.txt");
+var acro2classO = readTextFile("../static/acronym2classes/O.txt");
+var acro2classP = readTextFile("../static/acronym2classes/P.txt");
+var acro2classR = readTextFile("../static/acronym2classes/R.txt");
+var acro2classS = readTextFile("../static/acronym2classes/S.txt");
+var acro2classT = readTextFile("../static/acronym2classes/T.txt");
+var acro2classU = readTextFile("../static/acronym2classes/U.txt");
+var acro2classV = readTextFile("../static/acronym2classes/V.txt");
+var acro2classY = readTextFile("../static/acronym2classes/Z.txt");
 
 function readTextFile(file) {
 	var rawFile1 = new XMLHttpRequest();
@@ -50,87 +50,89 @@ function readTextFile(file) {
 	}
 }
 
-function loadClasses(dept2acro) {
+function loadClasses() {
+	int i = 0;
 	for (var dept in dept2acro) {
 		switch (dept[0]){
 			case 'A':
-				loadDepartment(dept2acro[dept], acro2classA);
+				i = loadDepartment(dept2acro[dept], acro2classA, i);
 				break;
 			case 'B':
-				loadDepartment(dept2acro[dept], acro2classB);
+				i = loadDepartment(dept2acro[dept], acro2classB, i);
 				break;
 			case 'C':
-				loadDepartment(dept2acro[dept], acro2classC);
+				i = loadDepartment(dept2acro[dept], acro2classC, i);
 				break;
 			case 'D':
-				loadDepartment(dept2acro[dept], acro2classD);
+				i = loadDepartment(dept2acro[dept], acro2classD, i);
 				break;
 			case 'E':
-				loadDepartment(dept2acro[dept], acro2classE);
+				i = loadDepartment(dept2acro[dept], acro2classE, i);
 				break;
 			case 'F':
-				loadDepartment(dept2acro[dept], acro2classF);
+				i = loadDepartment(dept2acro[dept], acro2classF, i);
 				break;
 			case 'G':
-				loadDepartment(dept2acro[dept], acro2classG);
+				i = loadDepartment(dept2acro[dept], acro2classG, i);
 				break;
 			case 'H':
-				loadDepartment(dept2acro[dept], acro2classH);
+				i = loadDepartment(dept2acro[dept], acro2classH, i);
 				break;
 			case 'I':
-				loadDepartment(dept2acro[dept], acro2classI);
+				i = loadDepartment(dept2acro[dept], acro2classI, i);
 				break;
 			case 'J':
-				loadDepartment(dept2acro[dept], acro2classJ);
+				i = loadDepartment(dept2acro[dept], acro2classJ, i);
 				break;
 			case 'K':
-				loadDepartment(dept2acro[dept], acro2classK);
+				i = loadDepartment(dept2acro[dept], acro2classK, i);
 				break;
 			case 'L':
-				loadDepartment(dept2acro[dept], acro2classL);
+				i = loadDepartment(dept2acro[dept], acro2classL, i);
 				break;
 			case 'M':
-				loadDepartment(dept2acro[dept], acro2classM);
+				i = loadDepartment(dept2acro[dept], acro2classM, i);
 				break;
 			case 'N':
-				loadDepartment(dept2acro[dept], acro2classN);
+				i = loadDepartment(dept2acro[dept], acro2classN, i);
 				break;
 			case 'O':
-				loadDepartment(dept2acro[dept], acro2classO);
+				i = loadDepartment(dept2acro[dept], acro2classO, i);
 				break;
 			case 'P':
-				loadDepartment(dept2acro[dept], acro2classP);
+				i = loadDepartment(dept2acro[dept], acro2classP, i);
 				break;
 			case 'R':
-				loadDepartment(dept2acro[dept], acro2classR);
+				i = loadDepartment(dept2acro[dept], acro2classR, i);
 				break;
 			case 'S':
-				loadDepartment(dept2acro[dept], acro2classS);
+				i = loadDepartment(dept2acro[dept], acro2classS, i);
 				break;
 			case 'T':
-				loadDepartment(dept2acro[dept], acro2classT);
+				i = loadDepartment(dept2acro[dept], acro2classT, i);
 				break;
 			case 'U':
-				loadDepartment(dept2acro[dept], acro2classU);
+				i = loadDepartment(dept2acro[dept], acro2classU, i);
 				break;
 			case 'V':
-				loadDepartment(dept2acro[dept], acro2classV);
+				i = loadDepartment(dept2acro[dept], acro2classV), i;
 				break;
 			case 'Y':
-				loadDepartment(dept2acro[dept], acro2classY);
+				i = loadDepartment(dept2acro[dept], acro2classY), i;
 				break;
-			}
+			default:
+				console.log('welp ur missing a letter');
 		}
 	}
 }
 
-function loadDepartment(acro, acro2class) {
-	int i = 0;
+function loadDepartment(acro, acro2class, i) {
 	// set background color to be something different for each department
 	for (var clas in acro2class[acronym]) {
 		document.getElementById('courses').innerHTML = "<div class='" + acronym + "' id='" + i + "'><h1>" + clas + "</h1></div>";
 		i++;
 	}
+	return i;
 }
 
 // animates tile once requirement is toggles
